2c19cdff4f5383908022a9f34963a33409ce18c2
Vijay Chandran <com.vjc@gmail.com>
2019-12-08 11:15:02 +0800

    protocols for weather details page

---
+++ WorldWeather/Models/WeatherModel.swift
@@ -0,0 +1,17 @@
+//
+//  Weather.swift
+//  WorldWeather
+//
+//  Created by v.a.jayachandran on 8/12/19.
+//  Copyright © 2019 v.a.jayachandran. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherModel {
+    let temperatureInCentigrade: Double
+    let temperatureInFahrenheit: Double
+    let humidity: Double
+    let weatherDescription: String?
+    let weatherIconURL: URL?
+}
---
+++ WorldWeather/Weather Details Page/WeatherDetailsPageProtocols.swift
@@ -0,0 +1,21 @@
+//
+//  SearchPageProtocols.swift
+//  WorldWeather
+//
+//  Created by v.a.jayachandran on 8/12/19.
+//  Copyright © 2019 v.a.jayachandran. All rights reserved.
+//
+
+import Foundation
+
+protocol WeatherDetailsPageInteractorInput {
+    
+}
+
+protocol WeatherDetailsPagePresenterInput {
+    func present(weather: WeatherModel)
+}
+
+protocol WeatherDetailsPagePresenterOutput {
+    func display(weather: WeatherModel)
+}
