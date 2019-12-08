9f12afc3852f4cb24b9cb27409f64d2b19375244
Vijay Chandran <com.vjc@gmail.com>
2019-12-08 15:02:17 +0800

    added Location

---
+++ WorldWeather/Models/Location.swift
@@ -0,0 +1,12 @@
+//
+//  Coordinate.swift
+//  WorldWeather
+//
+//  Created by v.a.jayachandran on 8/12/19.
+//  Copyright © 2019 v.a.jayachandran. All rights reserved.
+//
+
+struct Location {
+    let latitude: Double
+    let Longitude: Double
+}
